3281501b09882edc9f7b6881b85d8484988b59f8
mpone <m.parphenjuk@gmail.com>
2020-08-31 14:05:54 +0300

    Updates

---
+++ precache-manifest.4612963e1597a582c794f11d30e61002.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72caf2c27718c28ab9e30fc4e634b4dc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9dacc11bee8597c775e5",
+    "url": "./static/css/main.b96423e9.chunk.css"
+  },
+  {
+    "revision": "b0dff7fd6bb18487afb8",
+    "url": "./static/js/2.0ce2c40f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0ce2c40f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dacc11bee8597c775e5",
+    "url": "./static/js/main.115cbe05.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
